52c39eafdf16823d80797e3ca65259055dcb6157
carlosSploit <arturo14212000@gmail.com>
2020-10-22 06:23:45 -0500

    actualizar

--- view/js/Administracion.js
+++ view/js/Administracion.js
@@ -41,6 +41,12 @@ function principal(){
         $('#TituloModal').html("Lista de Pedidos")
         $('#ModalContainer').modal('show');
     });
+
+    $("#Admi").click(function (event){ //cuando se precione la opccion de meseng, cambia el contenedor
+        $('#contModal').html(ConfigAdmini);
+        $('#TituloModal').html("Registrar Administrador")
+        $('#ModalContainer').modal('show');
+    });
 }
 /*Vista del Categoria*/
 function ItenCatego(params) {
@@ -1261,4 +1267,86 @@ function CarritoCompra(){
     '           </div>'+
     '       </div>';
   }
+
+  function ConfigAdmini() {
+    return '<!----codico del modal----->'+
+    ' <div class="row">'+
+    ' <div class="col">'+
+        ' <div class="row">'+
+            ' <div class="col">'+
+                '<div class="row">'+
+                    '<div class="col-7">'+
+                        '<div class="row">'+
+                            '<div class="col">'+
+                                ' <div class="input-group mb-3">'+
+                                    ' <div class="input-group-prepend">'+
+                                        ' <span class="input-group-text" id="basic-addon1">🔢</span>'+
+                                        ' </div>'+
+                                    ' <input type="text" class="form-control" placeholder="Dni Administrador"'+
+                                    ' aria-label="Direccion" aria-describedby="basic-addon1">'+
+                                    ' </div>'+
+                                '</div>'+
+                            '</div>'+
+                        ' <div class="row">'+
+                            ' <div class="col">'+
+                                ' <div class="input-group mb-3">'+
+                                    ' <div class="input-group-prepend">'+
+                                        ' <span class="input-group-text" id="basic-addon1">📋</span>'+
+                                        ' </div>'+
+                                    ' <input type="text" class="form-control" placeholder="Nombre del Administrador"'+
+                                    ' aria-label="Direccion" aria-describedby="basic-addon1">'+
+                                    ' </div>'+
+                                ' </div>'+
+                            ' </div>'+
+                        ' <div class="row">'+
+                            ' <div class="col">'+
+                                ' <div class="input-group mb-3">'+
+                                    ' <div class="input-group-prepend">'+
+                                        ' <span class="input-group-text" id="basic-addon1">📱</span>'+
+                                        ' </div>'+
+                                        ' <input type="text" class="form-control" placeholder="Telefono del Administrador"'+
+                                        ' aria-label="Direccion" aria-describedby="basic-addon1">'+
+                                    ' </div>'+
+                                ' </div>'+
+                            ' </div>'+
+                        '</div>'+
+                    '<div class="col-5">'+
+                        '<div class="text-center">'+
+                            '<img src="./resorces/fondolo.jpg" style="width: 14vh; height: 14vh;'+
+                            '-webkit-box-shadow: 6px 4px 29px -14px rgba(0, 0, 0, 0.75);'+
+                            '-moz-box-shadow: 6px 4px 29px -14px rgba(0, 0, 0, 0.75);'+
+                            'box-shadow: 6px 4px 29px -14px rgba(0, 0, 0, 0.75);"'+
+                            ' class="rounded" alt="...">'+
+                            '</div>'+
+                        '<div class="row my-2">'+
+                            '<div class="col">'+
+                                ' <button type="button" style="text-align: center; width: 100px; height: 30px;" id="NewProdut"'+
+                                  '  class="btn btn-primary btn-block btn-sm mx-auto rounded-pill">Añadir' + 
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+                ' </div>'+
+            ' </div>'+
+        ' <div class="row">'+
+            ' <div class="col">'+
+                ' <div class="input-group mb-3">'+
+                    ' <div class="input-group-prepend">'+
+                        ' <span class="input-group-text" id="basic-addon1">📧</span>'+
+                        ' </div>'+
+                        ' <input type="text" class="form-control" placeholder="Correo Electronico"'+
+                        ' aria-label="Direccion" aria-describedby="basic-addon1">'+
+                    ' </div>'+
+                ' </div>'+
+            ' </div>'+
+        ' <div class="row">'+
+            ' <div class="col">'+
+                ' <button type="button" id="NewProdut" class="btn btn-success btn-block">Ingresar'+
+                    ' Datos</button>'+
+                ' </div>'+
+            ' </div>'+
+        ' </div>'+
+    ' </div>'+
+'<!------------------------->';
+}
 
